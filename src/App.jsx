ebb7dad1cab40a2e4ced8719371a160b0d254168
Michael Baxter <baxt01@hotmail.com>
2024-03-15 21:49:45 +0000

    I have taken the chance Hoping no one else from the team is online to commit at this time to tidy out the code such as package.jason had duplicate enteries so I have removed them and there was a slight overlap of code in the App.jsx fileso I removed the excess code lines now the page is loading and looks like it should leaving the styling such as element positioning to be corrected

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,17 @@ import { useState } from 'react'
 import Translate from './components/Translate.jsx'
 import './App.css'
 import Header from './components/Header'
-
-export default function App() {
-  const [count, setCount] = useState(0)
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Dictionary from "./component/Dictionary";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <Header />
       <Translate />
-     
+      <Dictionary />
+ 
     </>
   )
-}
-      <Dictionary />
-    </>
-  );
 }
