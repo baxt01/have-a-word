db9ef87b4e03f5af8ab2a8b158efba3b43dfeb6b
unknown <victoriasugden@yahoo.co.uk>
2024-03-15 13:29:01 +0000

    adding logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import jumbotronPic from '../../public/images/jumbotron.jpg'
+import logo from '../../public/images/logo.png'
 import "./Header.css"
 
 
@@ -10,9 +11,10 @@ export default function Header() {
       <div className="flex-row space-between px-1 header-jumbo">
         
         <img src={jumbotronPic} className="jumbo-image" alt="person reading" />
+        <div className="left"><img src={logo} className="logo-image" alt="Have a word logo" />
         <div className="centered"><h1>Welcome to Have a word!</h1></div>
       </div>
-    </div>
+    </div></div>
 
 
 
