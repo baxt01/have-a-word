1bb35ac06f6e345cae30d1e5ffb9b6a1429367ee
unknown <victoriasugden@yahoo.co.uk>
2024-03-15 12:12:40 +0000

    changes to image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import jumbotronPic from '../../public/images/jumbotron.jpg'
+import "./Header.css"
 
-export default function Header (){
-    return (
-    <div>
 
-<div className="jumbotron">
-  <h1 className="display-4">Hello, world!</h1>
-  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr className="my-4"/>
-  <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-  <p className="lead">
-    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div>
+export default function Header() {
+  return (
+    <div className="container-fluid">
 
+      <div className="flex-row space-between px-1 header-jumbo">
+        
+        <img src={jumbotronPic} className="jumbo-image" alt="person reading" />
+        <div className="centered"><h1>Welcome to Have a word!</h1></div>
+      </div>
     </div>
 
-)
+
+
+  )
 }
 
