51b2ed9781f47a45c3156c626d4af6dc474ac1b0
unknown <victoriasugden@yahoo.co.uk>
2024-03-16 15:42:24 +0000

    changes to header and created about component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Translate from './components/Translate.jsx'
 import './App.css'
 import Header from './components/Header'
 
+
+
 export default function App() {
   const [count, setCount] = useState(0)
   return (
     <>
       <Header />
       <Translate />
+     
      
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import jumbotronPic from '../../public/images/jumbotron.jpg'
+import logo from '../../public/images/logo.png'
 import "./Header.css"
+
 
 
 export default function Header() {
@@ -8,11 +10,17 @@ export default function Header() {
     <div className="container-fluid">
 
       <div className="flex-row space-between px-1 header-jumbo">
+      
+      <img src={jumbotronPic} className="jumbo-image" alt="person reading" />
+        <img src={logo} className="logo-image" alt="Have a word logo" />
+        <div className="centered"><h1>Welcome to Have a word!</h1>
+        <span><p><h4>A translation app, dictionary and learning quiz, all in one click.</h4></p></span></div>
         
-        <img src={jumbotronPic} className="jumbo-image" alt="person reading" />
-        <div className="centered"><h1>Welcome to Have a word!</h1></div>
+       </div>
+       <br></br>
+       <br></br>
       </div>
-    </div>
+    
 
 
 
