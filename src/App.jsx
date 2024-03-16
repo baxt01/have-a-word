71bc04682d9701b54d148981b8cd5429e95ec966
Michael Baxter <baxt01@hotmail.com>
2024-03-16 20:09:38 +0000

    fixing file system in the repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Translate from './components/Translate.jsx'
 import './App.css'
 import Header from './components/Header'
-import Dictionary from "./component/Dictionary";
+import Dictionary from "./components/Dictionary.jsx";
 
 export default function App() {
   const [count, setCount] = useState(0)
