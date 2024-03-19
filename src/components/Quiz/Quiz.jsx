60668560f60be6e97fe3be40e74b5c07e2e782a1
jeylanab <jeylantesi@gmail.com>
2024-03-19 05:50:38 +0000

    added usestate hook

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -7,4 +7,32 @@ const Quiz = ({ questions }) => {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [isCorrect, setIsCorrect] = useState(true);
   const [score, setScore] = useState(0);
+  
 
+  const handleAnswerSelect = (correctOption) => {
+    setSelectedAnswer(correctOption);
+  };
+
+  const handleNextQuestion = () => {
+    if (selectedAnswer !== null) {
+      const correctAnswer = questions[currentQuestion].correctOption;
+      const isAnswerCorrect = selectedAnswer === correctAnswer;
+
+    
+      setIsCorrect(isAnswerCorrect);
+
+     
+      if (isAnswerCorrect) {
+        setScore(score + 1);
+      }
+
+      if (currentQuestion + 1 < questions.length) {
+        setCurrentQuestion(currentQuestion + 1);
+        setSelectedAnswer(null);
+      } else {
+       
+      }
+    } else {
+      
+    }
+  };
