2eb95fc2eeb146f408c0a3de1e018c5482e56fd1
Tunde Folorunsho <foltunde@gmail.com>
2024-03-19 15:21:24 +0000

    create the navbar for the have a work the page responsive reutn the click and button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,42 +2,45 @@ import React from "react";
 import { ReactDOM } from 'react-dom/client';
 import "../styles/nav.css"
 
-function New(props) { 
+function Nav(props) {
     const {
-            pages = [], 
-            setCurrentPage,
-            currentPage,
+        pages = [],
+        setCurrentPage,
+        currentPage,
     } = props;
-}
-        useEffect(() => {
-            document.title = capitalizeFirstLetter(currentPage.name);
-        }, [currentPage]);
 
-            return (
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-                    <div className="container">
-                        <a className="navbar-brand js-scroll-trigger">HAVE-A-WORD </a>
-                        <button className="navbar-toggle navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-Toggle-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarResponsive">
-                            <ul className="navbar-nav ml auto">
-                                {pages.map((page)) => (
-                                    <li 
-                                    className={`nav-item nav-link js-scroll-trigger ${
-                                        currentPage.name === PageTransitionEvent.name && 'active'
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentPage.name);
+    }, [currentPage]);
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand js-scroll-trigger">Have-A-WORD</a>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        {pages.map((Page) => (
+                            <li
+                                className={`nav-item nav-link js-scroll-trigger ${
+                                    currentPage.name === Page.name && 'active'
                                     }`}
-                                    key={Page.name}
-                                    >
-                                       <span 
-                                       onClick={() => setCurrentPage(page)}
-                                       >
-                                         {capitalizeFirstLetter(page.name)}
-                                       </span>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            )
+                                key={Page.name}
+                            >
+                                <span
+                                    onClick={() => setCurrentPage(Page)}
+                                >
+                                    {capitalizeFirstLetter(Page.name)}
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
