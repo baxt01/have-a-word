bbed4f9936f3d681340f27c516fb91f4954a62c2
Almas Mansuri <almas.a.mansuri@gmail.com>
2024-03-19 16:56:31 +0000

    Dictionary app components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
-import { useState } from 'react'
-import Translate from './components/Translate.jsx'
-import './App.css'
-import Header from './components/Header'
-
-export default function App() {
-  const [count, setCount] = useState(0)
 import { useState } from "react";
+import Translate from "./components/Translate.jsx";
+import "./App.css";
+import Header from "./components/Header";
+import Dictionary from "./components/Dictionary";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-import Dictionary from "./component/Dictionary";
+import axios from "axios";
+import DictionaryComponent from "./components/DictionaryComponent.jsx";
 
 export default function App() {
+  const [data, setData] = useState();
+  const [word, setWord] = useState();
+  const [audio, setAudio] = useState();
+
+  const handleSearch = async (input) => {
+    const api = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+    const response = await axios.get(api + input);
+    console.log(input);
+    setData(response.data);
+    console.log(response.data);
+    setWord(input);
+
+    console.log(response.data[0].phonetics[0]);
+    if (response.data[0].phonetics[0] != undefined) {
+      setAudio(response.data[0].phonetics[0].audio);
+
+      console.log(response.data[0].phonetics[0].audio);
+    } else {
+      setAudio(undefined);
+    }
+  };
+
   return (
-    <>
+    <div>
+      <Navbar />
       <Header />
       <Translate />
-     
-    </>
-  )
-}
-      <Dictionary />
-    </>
+      <Dictionary handleSearch={handleSearch} />
+      {data && <DictionaryComponent data={data} word={word} audio={audio} />}
+    </div>
   );
 }
---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import "../style/Dictionary.css";
+
+export default function Dictionary({ handleSearch }) {
+  const [input, setInput] = useState("");
+
+  const handleClear = () => {
+    setInput("");
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <nav>
+          <h2>Dictionary App</h2>
+        </nav>
+      </header>
+      <main>
+        <form>
+          {/* <label htmlFor="search" className="form-label"> */}
+          <p>
+            Welcome to the Dictionary App Homepage! This App is designed to be
+            your go-to resource for all your language needs.
+          </p>
+          {/* </label> */}
+
+          <input
+            type="text"
+            // className="form-control"
+            placeholder="Enter a word"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="button1"
+            onClick={(event) => {
+              event.preventDefault();
+              handleSearch(input);
+            }}
+          >
+            Search
+          </button>
+          <button type="submit" className="button2" onClick={handleClear}>
+            Clear
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+}
