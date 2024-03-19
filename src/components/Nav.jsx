f3f1fdbc84b89c9d6edad20cf5001d345a85a83f
Tunde Folorunsho <foltunde@gmail.com>
2024-03-19 06:35:09 +0000

    create the scroll and pull

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,5 +14,30 @@ function New(props) {
         }, [currentPage]);
 
             return (
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top"
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                    <div className="container">
+                        <a className="navbar-brand js-scroll-trigger">HAVE-A-WORD </a>
+                        <button className="navbar-toggle navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-Toggle-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarResponsive">
+                            <ul className="navbar-nav ml auto">
+                                {pages.map((page)) => (
+                                    <li 
+                                    className={`nav-item nav-link js-scroll-trigger ${
+                                        currentPage.name === PageTransitionEvent.name && 'active'
+                                    }`}
+                                    key={Page.name}
+                                    >
+                                       <span 
+                                       onClick={() => setCurrentPage(page)}
+                                       >
+                                         {capitalizeFirstLetter(page.name)}
+                                       </span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             )
