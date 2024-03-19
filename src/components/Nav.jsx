198dd34de5d0e61b28584d8e4110994a1c62645c
Tunde Folorunsho <foltunde@gmail.com>
2024-03-19 06:04:58 +0000

    create the import links and functions for the navbar and return.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,18 @@
 import React from "react";
 import { ReactDOM } from 'react-dom/client';
 import "../styles/nav.css"
+
+function New(props) { 
+    const {
+            pages = [], 
+            setCurrentPage,
+            currentPage,
+    } = props;
+}
+        useEffect(() => {
+            document.title = capitalizeFirstLetter(currentPage.name);
+        }, [currentPage]);
+
+            return (
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top"
+            )
