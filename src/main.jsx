e1296f6e2e8577b82f2e63ee92a4b6330f80106e
Tunde Folorunsho <foltunde@gmail.com>
2024-03-21 13:05:51 +0000

    change the navbar setting

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './components/Nav.jsx'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import * as bootstrap from 'bootstrap';
 //  import 'dotenv/config'
